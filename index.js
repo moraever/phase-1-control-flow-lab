ea9bc54545e8e40eb1f9022ec24e9e184fad17e8
Ever <ever.m96@hotmail.com>
2023-04-05 07:40:01 -0400

    Control FLow lab complete.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    return "This one is on me!"
+  }
+  else if (distance > 400 && distance < 2000) {
+    return "That will be twenty bucks."
+  }
+  else if (distance > 2000 && distance < 2500 ) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (distance > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let response = city === `NYC` ? `Ok, sounds good.` : `No go.`
+  return response 
 }
 
-function switchOnCharmFromTip(){
+
+
+function switchOnCharmFromTip(tip){
+
+  switch(tip) {
+    case "generous":
+    return ("Thank you so much.")
+
+    case "not as generous":
+    return ("Thank you.")
+      
+    default:
+      return "Bye."
+      
+    
+  }
   // Write your code here!
 }
